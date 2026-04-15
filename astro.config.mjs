f8cb3e9a48792619fbdbf380d79946479b853a6b
coquantpaul-alt <coquant.paul@gmail.com>
2026-04-15 13:52:29 +0100

    Update astro.config.mjs
    To be able to preview site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://delfineo.com',
   integrations: [mdx()],
   i18n: {
     defaultLocale: 'en',
